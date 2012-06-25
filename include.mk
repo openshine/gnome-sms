442a75c012f5565451b9449917772d0a3a9958ce
César García Tapia <cesar.garcia.tapia@openshine.com>
2012-06-25 18:29:22 +0200

    Fixed a path error in the install directory

--- include.mk
+++ include.mk
@@ -1,6 +1,6 @@
 gschemabase = org.gnome.shell.extensions
 topextensiondir = $(datadir)/gnome-shell/extensions
-extensionname = sms 
+extensionname = sms
 uuid = $(extensionname)@openshine.com
 extensiondir = $(topextensiondir)/$(uuid)
 localextensiondir = $(HOME)/.local/share/gnome-shell/extensions/$(uuid)
